c9db86769052b5451eb43ebbf508322b3aa1ce14
Lewaa Bahmad <lewaabahmad@gmail.com>
2015-10-25 21:59:05 -0400

    done; ready for design

--- app/models/art.rb
+++ app/models/art.rb
@@ -1,2 +1,9 @@
 class Art < ActiveRecord::Base
+  def venmo_url
+    "https://venmo.com/?txn=pay&recipients=rocio-cabrera-1&amount=#{self.price}&note=#{self.venmo_note}&audience=private"
+  end
+
+  def venmo_note
+    "Payment for #{self.title}".gsub(" ", "%20")
+  end
 end
