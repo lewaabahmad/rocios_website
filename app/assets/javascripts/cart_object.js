cb4a19835f6fdb445097a28b17e183be2de45e6d
Lewaa Bahmad <lewaabahmad@gmail.com>
2016-01-12 22:03:07 -0500

    refactor js

--- app/assets/javascripts/cart_object.js
+++ app/assets/javascripts/cart_object.js
@@ -1,26 +1,27 @@
 Rocio.cart = (function() {
 
   return {
+//INITIALIZER
     initialize: function() {
+      //set cart to empty array if there is none
       if (localStorage.getItem('cart') === null) {
         localStorage.setItem('cart', JSON.stringify([]));
       }
-      this.updateCartIcon()
+      //update the cart icon
+      this.updateCartIcon();
+      //add add to cart event listeners
+      this.addAddToCartEventListeners();
     },
 
+//CRUD OPERATIONS
+  //C
+    set: function(cart) {
+      localStorage.setItem('cart', cart);
+    },
+
+  //R
     get: function() {
       return JSON.parse(localStorage.getItem('cart'));
-    },
-
-    set: function(cart) {
-      localStorage.setItem('cart', cart)
-    },
-
-    removeItem: function(item) {
-      var cart = this.get();
-      debugger
-      //this will need to rmeove the item from the array of 
-      localStorage.setItem('cart', JSON.stringify(cart));
     },
 
     getArtTitles: function() {
@@ -28,20 +29,14 @@ Rocio.cart = (function() {
       for (title in Rocio.art) {
         artPieces.push(title);
       }
-      return artPieces
+      return artPieces;
     },
 
-    sanitizeCart: function() {
-      var cart = this.get();
-      var artTitles = this.getArtTitles();
-      for (var cartPiece in cart) {
-        if (artTitles.indexOf(cartPiece) === -1) {
-          cart = cart.splice(cart.indexOf(cartPiece) , 1);
-        }
-      }
-      this.set(cart);
+    length: function() {
+      return this.get().length;
     },
 
+  //U
     add: function(piece) {
       var cart = this.get();
       cart.push(piece);
@@ -57,73 +52,37 @@ Rocio.cart = (function() {
       }
     },
 
-    length: function() {
-      return this.get().length;
+//CART MAINTENANCE 
+    sanitizeCart: function() {
+      var cart = this.get();
+      var artTitles = this.getArtTitles();
+      for (var cartPiece in cart) {
+        if (artTitles.indexOf(cartPiece) === -1) {
+          cart = cart.splice(cart.indexOf(cartPiece) , 1);
+        }
+      }
+      this.set(cart);
     },
 
+//CART ICON
     updateCartIcon: function() {
       document.getElementById("cart-nav").innerHTML = "cart - " + this.length();
+    }, 
+
+//ART PAGE - ADD TO CART
+    addAddToCartEventListeners: function () {
+      var addToCartButtons = document.getElementsByClassName("add-to-cart");
+      for (var i = 0; i < addToCartButtons.length; i++) {
+        var self = this;
+        addToCartButtons[i].addEventListener("click", function(event) {
+          debugger
+          self.updateCartIcon();
+        })
+      }
     }
   }
+
+//CART PAGE 
 })()
 
 Rocio.cart.initialize()
-
-
-
-
-
-
-
-
-  // function generateEmptyCart() {
-  //   var emptyCart = "{";
-  //   for (var i = 0; i < limit; i++) {
-  //     emptyCart += '"' + artTitles[i] + '"' + ": false";
-  //     if (i != limit - 1) {
-  //       emptyCart += ",";
-  //     }
-  //   }
-  //   emptyCart += "}";
-  //   return emptyCart;
-  // }
-
-  // function aPieceHasNotBeenFound(index) {
-  //   artData[artTitles[index]] === undefined
-  // }
-
-  // function cartInvalid(limit, artTitles) {
-  //   // piece not found
-  //   // debugger
-  //   for (var i = 0; i < limit; i++) {
-  //     if (aPieceHasNotBeenFound(i)) {
-  //       return true;
-  //     }
-  //   }
-  //   // item added or removed
-  //   if (localStorage.getItem('cart') !== null) {
-  //     if (localStorage.getItem('cart').split(",").length !== artCount) {
-  //       return true;
-  //     }
-  //   }
-  //   return false;
-  // }
-
-  // if (localStorage.getItem('cart') === null) {
-  //   var emptyCart = generateEmptyCart();
-  //   localStorage.setItem('cart', emptyCart);    
-  // }
-
-  // if (cartInvalid(limit, artTitles)) {
-  //   var emptyCart = generateEmptyCart();
-  //   var currentCart = JSON.parse(localStorage.getItem('cart'));
-  //   for (var i = 0; i < limit; i++) {
-  //     if (currentCart[artTitles[i]] === true && emptyCart[artTitles[i]] === false) {
-  //       //same problem here
-  //       emptyCart[artTitles[i]] = true;
-  //     }
-  //   }
-  //   localStorage.clear()
-  //   localStorage.setItem('cart', emptyCart);
-  // } 
-  // }
