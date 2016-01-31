faf51ed6b9d4c809c0e41c8b461945a5157a4051
lbahmad <lbahmad@xogrp.com>
2016-01-31 17:47:34 -0500

    flushing out stripe front end

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ Bundler.require(*Rails.groups)
 
 module Rocio
   class Application < Rails::Application
+
+    config.assets.precompile += %w( jquery.payment.js )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
