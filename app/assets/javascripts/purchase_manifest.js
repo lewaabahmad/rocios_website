e05d2fc90fd120a9395e5d55e103173692c26284
Lewaa Bahmad <lewaabahmad@gmail.com>
2016-02-27 12:19:31 -0500

    refactoring

---
+++ app/assets/javascripts/purchase_manifest.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require purchase_stripe_manager.js 
+//= require purchase_validation_manager.js 
---
+++ app/assets/javascripts/site_manifest.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require art_data
+//= require cart_object 
+//= require checkout_modal
+//= require payment_form_submittor
+//= require stars
